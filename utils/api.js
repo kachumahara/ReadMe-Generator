3969d9c5e8ccc163a3c235d4d483cd36eec3fba1
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-02-27 20:48:21 -0700

    got my github api

--- utils/api.js
+++ utils/api.js
@@ -5,7 +5,7 @@ const api = {
 	getUser(username) {
 		return axios
 			.get(
-				`https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+				`https://api.github.com/users/${username}`
 			)
 			.catch(erroe => {
 				console.log("no user", err);
